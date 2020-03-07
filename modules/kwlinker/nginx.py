e72e9e9643e4b85a172f143ca78eb0d8b85dadef
Michael Lustfield <michael@lustfield.net>
2020-03-07 15:09:56 -0600

    Update link

--- modules/kwlinker/nginx.py
+++ modules/kwlinker/nginx.py
@@ -6,5 +6,5 @@ def linker(self, lexer, stream, options):
     for ttype, value in stream:
         value = value.replace('_LNK_', '_LNK__')
         if ttype is Token.Keyword or ttype is Token.Keyword.Namespace:
-            value = '_LNK_B_ngxbot.nginx.org/r/%s_LNK_M_%s_LNK_E_' % (value, value)
+            value = '_LNK_B_nginx.org/r/%s_LNK_M_%s_LNK_E_' % (value, value)
         yield ttype, value
